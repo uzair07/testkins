effd70ef0677707c482b6b2e91bd10572b732e77
uzair07 <mohammeduzair07@gmail.com>
2020-11-05 13:06:47 +0000

    Change

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World from Flask"
+    return "Hello World from k"
 
 if __name__ == "__main__":
     # Only for debugging while developing
